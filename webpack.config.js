5ea297ba125565a1c1bad33fc0a8674456f4e1ba
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-19 01:38:49 -0300

    Add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
