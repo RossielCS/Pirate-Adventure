b24c4fd81bdde068ad4f1330abc94ad9c9005d14
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-24 06:40:58 -0300

    Add methods to work with api
    - Add getScores
    - Add postScore
    - Add errHandler

--- src/modules/game-score.js
+++ src/modules/game-score.js
@@ -8,4 +8,35 @@ const validateInput = (input) => {
   return false;
 };
 
+const errHandler = () => new Response(JSON.stringify({ message: 'City not found.' }));
 
+const postScore = async (data) => {
+  let response = '';
+  if (data) {
+    response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1jFd06GQ0jNaUanltu4w/scores/', { 
+      mode: 'cors',
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).catch(errHandler);
+  }
+  const score = await response.json();
+  if (response === 'Leaderboard score created correctly.') {
+    return score;
+  }
+  return false;
+};
+
+const getScores = async (validation) => {
+  let response = '';
+  if (validation) {
+    response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1jFd06GQ0jNaUanltu4w/scores/', { mode: 'cors', method: 'GET' }).catch(errHandler);
+  }
+  const scores = await response.json();
+  if (response === 'Leaderboard score created correctly.') {
+    return scores;
+  }
+  return false;
+};
