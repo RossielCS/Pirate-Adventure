e98c85028c6ce72ecd8c599c970cad4d145658c8
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-26 12:15:19 -0300

    Add more unit tests for model

---
+++ src/test/game-score.test.js
@@ -0,0 +1,7 @@
+import {
+  validateInput, postScore, getScores, sortScores,
+} from '../modules/game-score';
+
+describe('Game Score', () => {
+  
+});
--- src/test/model.test.js
+++ src/test/model.test.js
@@ -2,7 +2,7 @@ import Model from '../modules/model';
 
 describe('Model', () => {
   const model = new Model();
-  const scores = [
+  const testScores = [
     { user: 'Alan', score: 680 },
     { user: 'Jessica', score: 970 },
   ];
@@ -19,5 +19,44 @@ describe('Model', () => {
   test('It should change the value of musicOn to false', () => {
     model.musicOn = false;
     expect(model.musicOn).toBe(false);
+  });
+
+  test('It should return false if the background music is off', () => {
+    expect(model.bgMusicPlaying).toBe(false);
+  });
+
+  test('It should change the value of bgMusicPlaying', () => {
+    model.bgMusicPlaying = true;
+    expect(model.bgMusicPlaying).toBe(true);
+  });
+
+  test('It should return zero witch if the user has not acumulated points', () => {
+    expect(model.score).toBe(0);
+  });
+
+  test('It should change the value of scores', () => {
+    model.score += 10;
+    expect(model.score).toBe(10);
+  });
+
+  test('It should return false if the user has not played the game', () => {
+    expect(model.played).toBe(false);
+  });
+
+  test('It should change the value of played to true', () => {
+    model.played = true;
+    expect(model.played).toBe(true);
+  });
+
+  test('It should return null if is has not been saved user\'s scores', () => {
+    expect(model.allScores).toBe(null);
+  });
+
+  test('It should change allScores value to an array of objects', () => {
+    model.allScores = [
+      { user: 'Alan', score: 680 },
+      { user: 'Jessica', score: 970 },
+    ];
+    expect(model.allScores).toEqual(testScores);
   });
 });
