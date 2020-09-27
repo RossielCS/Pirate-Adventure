472d19934c1303e98bdcd4f33c69536c2072f967
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-27 03:45:05 -0300

    Add unit tests fot game score module
    - validateInput
    - sortScores

--- src/test/game-score.test.js
+++ src/test/game-score.test.js
@@ -21,4 +21,30 @@ describe('Game Score', () => {
     input.score = 740;
     expect(validateInput(input)).toBe(false);
   });
+
+  test('It should return false if the value of user name it has a length less than three', () => {
+    const input = {};
+    input.user = 'Lo';
+    input.score = 740;
+    expect(validateInput(input)).toBe(false);
+  });
+
+  test('It should return false if the value of user score is equal to zero', () => {
+    const input = {};
+    input.user = 'Martha';
+    input.score = 0;
+    expect(validateInput(input)).toBe(false);
+  });
+
+  test('It should return -1 if a is greater than b', () => {
+    expect(sortScores(10, 5)).toBe(-1);
+  });
+
+  test('It should return 0 if a is equal to b', () => {
+    expect(sortScores(5, 5)).toBe(0);
+  });
+
+  test('It should return 1 if a is less than b', () => {
+    expect(sortScores(10, 15)).toBe(1);
+  });
 });
